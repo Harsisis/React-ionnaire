18ded69bb620627618811ec2fe5e9911caaea12a
Haorsis <cadetgauthier3@gmail.com>
2022-05-04 17:42:50 +0200

    no message

--- src/04-color-viewer/ColorViewer.js
+++ src/04-color-viewer/ColorViewer.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import classes from './ColorViewer.module.css';
+import { BlockPicker } from 'react-color';
 
 function ColorViewer() {
+  const [state, setState] = useState("#000");
+  const handleChangeComplete = (color) => {
+    setState(color.hex);
+    console.log(state);
+  };
+  const colorStyle= {
+    backgroundColor: {state},
+  }
   return (
-    <>
+    <div>
       <h2>Color viewer</h2>
       <form>
-        <input />
-        <button type="submit">Submit</button>
+        <BlockPicker
+          color={ state }
+          onChangeComplete={ handleChangeComplete }
+        />
       </form>
-      <div className={classes.box} />
-    </>
+      <div className={classes.box} style={colorStyle}></div>
+    </div>
   );
 }
 
